992deb8d4f4c7d459df7f2c7a19de871026ed8dc
josh <joshrandall8478@gmail.com>
2023-02-23 00:52:21 -0500

    Added scroll/hiding functionality

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 })
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        return document.querySelector('.navbar').classList.add('hide')
+    }
+    return document.querySelector('.navbar').classList.remove('hide')
+})
