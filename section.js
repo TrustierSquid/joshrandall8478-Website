f105f07953a1923392302e6b421a77b67cfad48f
Samuel <enthunt.sh@gmail.com>
2023-02-28 18:53:38 -0500

    Added forEach function to new script tag

--- section.js
+++ section.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('.project-btn').addEventListener('click', displayProjectHeader);
+let projectBtn =  document.querySelectorAll("project-btn");
 
+projectBtn.forEach((button) => {
+    projectBtn.addEventListener("click", () => {
+        document.getElementById("project-name-holder").innerHTML = 'hi';
+        console.log("Hello, " + projectBtn.innerHTML);
+    })
+})
 
-function displayProjectHeader(project){
-    console.log(project);
-}
+console.log("Hello, " + projectBtn.innerHTML);
