b53b89e7db3ad3a34a71535f89535c099897cf13
Samuel <enthunt.sh@gmail.com>
2023-03-04 14:55:53 -0500

    Added button focus

--- section.js
+++ section.js
@@ -1,10 +1,52 @@
-let projectBtn =  document.querySelectorAll("project-btn");
 
-projectBtn.forEach((button) => {
-    projectBtn.addEventListener("click", () => {
-        document.getElementById("project-name-holder").innerHTML = 'hi';
-        console.log("Hello, " + projectBtn.innerHTML);
+
+
+let projectBtn =  document.querySelectorAll(".project-btn");
+
+
+// #77DD7
+let btnIndicator = projectBtn;
+
+
+function projectClicks(){
+    projectBtn[0].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 1';
+        console.log("Project 1 clicked");
+
+        
+        
     })
-})
+    
+    projectBtn[1].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 2';
+        console.log("Project 2 clicked");
 
-console.log("Hello, " + projectBtn.innerHTML);
+    })
+    
+    projectBtn[2].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 3';
+        console.log("Project 3 clicked");
+
+    })
+    
+    projectBtn[3].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 4';
+        console.log("Project 4 clicked");
+        
+    })
+    
+    projectBtn[4].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 5';
+        console.log("Project 5 clicked");
+
+    })
+
+    projectBtn[5].addEventListener("click", () =>{
+        document.getElementById("project-name-holder").innerHTML = 'Project 6';
+        console.log("Project 6 clicked");
+
+    })
+    
+}
+
+projectClicks()
---
+++ test.js
@@ -0,0 +1,11 @@
+
+let arr = ['red', 'blue', 'green', 'yellow'];
+
+
+function borderChange(button){
+    for(let i = 0; i == button;) {
+        console.log(i)
+    }    
+}
+
+borderChange(arr);
